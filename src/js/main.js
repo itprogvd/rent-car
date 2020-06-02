bca08f643d959707e62fe0ac62d1ab3ece46cd46
dudashvictor <viktordudash36@gmail.com>
2020-06-02 21:15:53 +0300

    feat: add two sections

--- src/js/main.js
+++ src/js/main.js
@@ -26,4 +26,57 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   });
 
+
+  var mySwiper = new Swiper('.videos-wrap', {
+    loop: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev'
+    },
+    slidesPerView: 2,
+    spaceBetween: 10,
+    // Responsive breakpoints
+    breakpoints: {
+      // when window width is >= 320px
+      320: {
+        slidesPerView: 1,
+        // 
+      },
+      // when window width is >= 480px
+      480: {
+        slidesPerView: 1,
+        // spaceBetween: 20
+      },
+      // when window width is >= 640px
+      767: {
+        slidesPerView: 2,
+        // spaceBetween: 30
+      }
+    }
+  });
+
+  var mySecondSwiper = new Swiper('.about-slider-wrap', {
+    loop: true,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev'
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'fraction'
+    },
+    slidesPerView: 1,
+    renderFraction: function (currentClass, totalClass) {
+      return '<span class="' + currentClass + '"></span>' + ' of ' + '<span class="' + totalClass + '"></span>';
+    }
+  });
+
+  // var next = $('.swiper-button-next');
+  // var prev = $('.swiper-button-prev');
+  // var bullets = $('.swiper-pagination');
+
+  // next.css('left', prev.width() + 20 + bullets.width() + 20);
+  // bullets.css('left', prev.width() + 20);
+
+  new WOW().init();
 });
