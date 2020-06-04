89bd0939a3512bf0b45225076866dc77b8ddf5f2
dudashvictor <viktordudash36@gmail.com>
2020-06-04 16:42:38 +0300

    fixed dropdown

--- src/js/main.js
+++ src/js/main.js
@@ -27,6 +27,64 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 
 
+  /* Select styles */
+  $('.select').each(function () {
+    const _this = $(this),
+      selectOption = _this.find('option'),
+      selectOptionLength = selectOption.length,
+      selectedOption = selectOption.filter(':selected'),
+      duration = 450; // длительность анимации 
+
+    _this.hide();
+    _this.wrap('<div class="select"></div>');
+    $('<div>', {
+      class: 'new-select',
+      text: _this.children('option:disabled').text()
+    }).insertAfter(_this);
+
+    const selectHead = _this.next('.new-select');
+    $('<div>', {
+      class: 'new-select__list'
+    }).insertAfter(selectHead);
+
+    const selectList = selectHead.next('.new-select__list');
+    for (let i = 1; i < selectOptionLength; i++) {
+      $('<div>', {
+          class: 'new-select__item',
+          html: $('<span>', {
+            text: selectOption.eq(i).text()
+          })
+        })
+        .attr('data-value', selectOption.eq(i).val())
+        .appendTo(selectList);
+    }
+
+    const selectItem = selectList.find('.new-select__item');
+    selectList.slideUp(0);
+    selectHead.on('click', function () {
+      if (!$(this).hasClass('on')) {
+        $(this).addClass('on');
+        selectList.slideDown(duration);
+
+        selectItem.on('click', function () {
+          let chooseItem = $(this).data('value');
+
+          $('select').val(chooseItem).attr('selected', 'selected');
+          selectHead.text($(this).find('span').text());
+          selectHead.text(window.getComputedStyle($('new-select__item'), ':before').content);
+
+          selectList.slideUp(duration);
+          selectHead.removeClass('on');
+        });
+
+      } else {
+        $(this).removeClass('on');
+        selectList.slideUp(duration);
+      }
+    });
+  });
+
+
   var mySwiper = new Swiper('.videos-wrap', {
     loop: true,
     navigation: {
@@ -94,6 +152,34 @@ document.addEventListener('DOMContentLoaded', () => {
         scrollTop: 0
       }, 500);
       return false;
+    });
+  });
+
+  $('.order__form').validate({
+    errorElement: 'div',
+    errorClass: 'invalid',
+    rules: {
+      phone: {
+        required: true,
+        minlength: 17
+      }
+    },
+    messages: {
+      phone: {
+        required: "Укажите телефон",
+        minlength: "Телефон некорректный"
+      }
+    }
+  });
+
+  $('[type=tel]').on('focus', () => {
+    $('[type=tel]').mask('+7(000) 000-00-00', {
+      placeholder: "+7 (___) __-__-___"
+    });
+  });
+  $('[type=tel]').on('blur', () => {
+    $('[type=tel]').mask('+7(000) 000-00-00', {
+      placeholder: "Ваш телефон"
     });
   });
 
